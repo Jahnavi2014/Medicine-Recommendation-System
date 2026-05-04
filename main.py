b22b924e3825e72ed9040ac9eb5506d2a2a3ee10
Jahnavi Chowdhary <jahnavijanu836@gmail.com>
2026-05-04 11:23:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -114,7 +114,12 @@ def developer():
 def blog():
     return render_template("blog.html")
 
-
 if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
 
-    app.run(debug=True)
+
+# if __name__ == '__main__':
+
+#     app.run(debug=True)
+
+
